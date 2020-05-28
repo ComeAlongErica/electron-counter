fc154c326f5c6f7c4b7ede0508767046f73991b5
Erica Perry <erica.perry@woodlanddirect.com>
2020-05-28 12:46:41 -0400

    add: garabge clean up

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+let mainWindow
+
 app.on('ready', _ => {
-  new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 400,
     width: 400
+  })
+
+  // null out on close for garbage collection
+  mainWindow.on('closed', _ => {
+    console.log('closed')
+    mainWindow = null
   })
 })
